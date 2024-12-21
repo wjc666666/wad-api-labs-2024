367738d97c10bd9db26539f29c2bff92f7c2e1aa
wjc666666 <2101079563@qq.com>
2024-12-21 22:31:19 +0000

    Mongo lab; Included Mongoose

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,74 +1,43 @@
 import express from 'express';
-import { tasksData } from './taskData';
-import { v4 as uuidv4 } from 'uuid';
+import Task from './taskModel';
 
-const router = express.Router();
+const router = express.Router(); // eslint-disable-line
 
-
-router.get('/', (req, res) => {
-    res.json(tasksData);
+// Get all tasks
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
 });
 
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const task = tasksData.tasks.find(task => task.id === id);
-    if (!task) {
-        return res.status(404).json({ status: 404, message: 'Task not found' });
+// create a task
+router.post('/', async (req, res) => {
+    const task = await Task(req.body).save();
+    res.status(201).json(task);
+});
+// Update Task
+router.put('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Task.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if (result.matchedCount) {
+        res.status(200).json({ code:200, msg: 'Task Updated Successfully' });
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
-    return res.status(200).json(task);
 });
 
-
-router.post('/', (req, res) => {
-    const { title, description, deadline, priority, done } = req.body;
-    const currentTime = new Date().toISOString();
-    const newTask = {
-        id: uuidv4(),
-        title,
-        description,
-        deadline,
-        priority,
-        done,
-        created_at: currentTime,
-        updated_at: currentTime
-    };
-    tasksData.tasks.push(newTask);
-    tasksData.total_results++;
-    res.status(201).json(newTask);
-});
-
-
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
-    if (taskIndex === -1) {
-        return res.status(404).json({ status: 404, message: 'Task not found' });
+// delete Task
+router.delete('/:id', async (req, res) => {
+    if (req.body._id) delete req.body._id;
+    const result = await Task.deleteOne({
+        _id: req.params.id,
+    });
+    if (result.deletedCount) {
+        res.status(204).json();
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
     }
-
-    const currentTime = new Date().toISOString();
-    const updatedTask = {
-        ...tasksData.tasks[taskIndex],
-        ...req.body,
-        id, 
-        updated_at: currentTime 
-    };
-
-    tasksData.tasks[taskIndex] = updatedTask;
-    res.json(updatedTask);
-});
-
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
-
-    if (taskIndex === -1) {
-        return res.status(404).json({ status: 404, message: 'Task not found' });
-    }
-    tasksData.tasks.splice(taskIndex, 1);
-    tasksData.total_results--;
-    res.status(204).send();
 });
 
 export default router;
